fad98cef75ad6d43568a07cf50760a7fd148a0e5
Mathieu Dombrock <dombrock.mathieu@gmail.com>
2021-06-05 00:15:46 +0000

    buffer -> data

--- uploadData.js
+++ uploadData.js
@@ -1,13 +1,13 @@
 const s3 = require('./s3');
 
-module.exports = (bucket, buffer, fileName)=>{
+module.exports = (bucket, data, fileName)=>{
   // call S3 to retrieve upload file to specified bucket
   let uploadParams = {Bucket: bucket, Key: '', Body: ''};
   // call S3 to retrieve upload file to specified bucket
   return new Promise((resolve, reject)=>{
     try{
       // Configure the file stream and obtain the upload parameters
-      uploadParams.Body = buffer;
+      uploadParams.Body = data;
       const path = require('path');
       uploadParams.Key = path.basename(fileName);
       s3.upload (uploadParams, function (err, data) {
