b556a7896143292b2fa7a2a9129faae3a8b2127c
Mathieu Dombrock <dombrock.mathieu@gmail.com>
2021-11-02 04:10:41 -0700

    Reformat code

--- ns3.js
+++ ns3.js
@@ -29,7 +29,7 @@ node ns3 listItems BUCKET PREFIX(optional)
 `;
 
 async function run(){
-  const flag = process.argv[2];
+  const flag = ;
   let argOff = 0;// arg offset
   let outputFile = false;
   if(flag){
@@ -38,29 +38,43 @@ async function run(){
       argOff = 1;
     }
   }
+  const args = [
+    process.argv[0],
+    process.argv[1],
+    process.argv[2+argOff],
+    process.argv[3+argOff],
+    process.argv[4+argOff],
+    process.argv[5+argOff],
+  ];
   const cmd = process.argv[2+argOff];
   let res = "";
-
   switch(cmd){
     case 'download':
       if(!isValid(5+argOff)){
         break;
       }
       console.log('Downloading file');
-      res = await download(process.argv[3+argOff], process.argv[4+argOff], process.argv[5+argOff]);
+      const bucket = process.argv[3+argOff];
+      const key = process.argv[4+argOff];
+      const destination = process.argv[5+argOff];
+      res = await download(bucket, key, destination);
       break;
     case 'upload':
       if(!isValid(4+argOff)){
         break;
       }
       console.log('Uploading file');
-      res = await uploadFile(process.argv[3+argOff], process.argv[4+argOff]);
+      const bucket = process.argv[3+argOff];
+      const target = process.argv[4+argOff];
+      res = await uploadFile(bucket, target);
       break;
     case 'listItems':
       if(!isValid(3+argOff)){// arg 4 is optional
         break;
       }
-      res = await listItems(process.argv[3+argOff], process.argv[4+argOff]);
+      const bucket = process.argv[3+argOff];
+      const prefix = process.argv[4+argOff];
+      res = await listItems(bucket, prefix);
       break;
     case 'listBuckets':
       res = await listBuckets();
@@ -70,7 +84,8 @@ async function run(){
         break;
       }
       console.log('Creating Bucket');
-      res = await create(process.argv[3+argOff]);
+      const bucket = process.argv[3+argOff];
+      res = await create();
       break;
     case 'help':
       console.log(helpText);
